b8e3546781c7f3550707fc3f239da68a94aa477a
dan <dan@LAPTOP-P5495NV9>
2017-03-24 14:06:05 +0100

    Setting comment

--- app/Models/Helper.php
+++ app/Models/Helper.php
@@ -4,63 +4,90 @@ namespace App\Models;
 
 // this class retrun static method for useful tasks provided in the template
 
-class Helper 
-{
-  
-    public function __contruct(){}
-    
-    public static function normalizeFabricName($name){
-      return substr($name,9);        
-    }
-    
-    public static function formatCurrency( $amount ){
+class Helper {
+
+    public function __contruct() {
         
-        $format = $amount; 
-       
-        switch ($amount){   
+    }
+
+    public static function normalizeFabricName($name) {
+        return substr($name, 9);
+    }
+
+    
+    /**
+     * formatCurrency thai currency
+     *
+     * @param  Integer  $amount 
+     * @return String
+     */
+    public static function formatCurrency($amount) {
+
+        $format = $amount;
+
+        switch ($amount) {
             case $amount < 999:
-                $format = $amount.",00";
-            break;
+                $format = $amount . ",00";
+                break;
             case $amount > 1000 && $amount < 9999:
-                $format = substr($amount,0,1).",".substr($amount,1);
-            break;
+                $format = substr($amount, 0, 1) . "," . substr($amount, 1);
+                break;
             case $amount > 9999 && $amount < 99999:
-                $format = substr($amount,0,2).",".substr($amount,2);
-            break;
+                $format = substr($amount, 0, 2) . "," . substr($amount, 2);
+                break;
             case $amount > 99999 && $amount < 999999:
-                $format = substr($amount,0,3).",".substr($amount,3);
-            break;
+                $format = substr($amount, 0, 3) . "," . substr($amount, 3);
+                break;
             case $amount > 999999 && $amount < 9999999:
-                 $format = substr($amount,0,1).",".substr($amount,0,3).",".substr($amount,5);
-            break;
-            
-        }       
-        
-       
-        if($amount < 999){ $format = $amount.",00";}         
-        
-        return $format;        
+                $format = substr($amount, 0, 1) . "," . substr($amount, 0, 3) . "," . substr($amount, 5);
+                break;
+        }
+
+
+        if ($amount < 999) {
+            $format = $amount . ",00";
+        }
+
+        return $format;
     }
-    
-    
-    public static function calculateDiscount( $amount , $discount ){              
-             
+
+    /**
+     * Calcultate discount
+     *
+     * @param  Integer  $amount 
+     * @param  Integer  $discount 
+     * @return Integer
+     */
+    public static function calculateDiscount($amount, $discount) {
+
         $discount = (100 - $discount) / 100;
         return round($amount * $discount);
     }
     
-
     
-    public static function sum( $amount1 , $amount2 ){
-        
+    /**
+     * Calcultate sum
+     *
+     * @param  Integer  $amount1 
+     * @param  Integer  $amount2 
+     * @return Integer
+     */
+
+    public static function sum($amount1, $amount2) {
+
         return round($amount1 + $amount2);
     }
+
     
-    public static function findCategoryNameById($id){
-      $category = \App\Models\Category\Category::findCategoryById($id); 
-      return $category->name;
-      
+    /**
+     * Calcultate sum
+     *
+     * @param  Integer  $id 
+     * @return Category object
+     */
+    public static function findCategoryNameById($id) {
+        $category = \App\Models\Category\Category::findCategoryById($id);
+        return $category->name;
     }
-   
-      
+
 }
